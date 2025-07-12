ee2385c8c449065d18aab72c2a5c5d84b22b365b
sreeraj.mv <sreeraj.mv@gmail.com>
2025-07-12 15:06:34 +0530

    updated the config

--- routes/order/order.py
+++ routes/order/order.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 from dotenv import load_dotenv  # type: ignore
 import os
 import requests # type: ignore
-import pyodbc
+# import pyodbc
 
 order_bp = Blueprint("order", __name__)
 
@@ -36,9 +36,9 @@ def get_order():
     try:
         # Fetch and validate query parameters
         order_id = request.args.get("order_id")
-        user_id = request.args.get("user_id")
-        if not (order_id and order_id.isdigit() and user_id and user_id.isdigit()):
-            return jsonify({"message": "Missing or invalid query parameters."}), 400
+        cardcode = request.args.get("cardcode")
+        # if not (order_id and order_id.isdigit() and user_id and user_id.isdigit()):
+        #     return jsonify({"message": "Missing or invalid query parameters."}), 400
         # SQL query to fetch order details
         query = """
             SELECT  
@@ -68,12 +68,11 @@ def get_order():
                 TBL_SalesOrderDetails AS A
             LEFT JOIN CustomerMaster_M_Tbl AS B ON A.CustCode = B.CardCode
             LEFT JOIN LocationMaster_M_Tbl AS C ON A.LocationID = C.LocationId
-            LEFT JOIN TBL_Users AS D ON D.UserID = A.MakerId
             WHERE 
-                A.Status = 'N' AND A.SlNo = ? AND D.Usr_Name = ?
+                A.Status IN ('N' ,'P') AND A.SlNo = ? AND B.CardCode = ?
             FOR JSON PATH, INCLUDE_NULL_VALUES;
         """
-        params = (order_id, user_id)
+        params = (order_id, cardcode)
         # Execute the query and fetch the result
         orderdetails = ms_query_db(query, params)
         # Process the result
@@ -92,6 +91,69 @@ def get_order():
     except Exception as e:
         print(f"Error: {e}")
         return jsonify({"message": "Internal server error."}), 500
+    
+
+# @order_bp.route("/order", methods=["GET"])
+# def get_order():
+#     try:
+#         # Fetch and validate query parameters
+#         order_id = request.args.get("order_id")
+#         user_id = request.args.get("user_id")
+#         # if not (order_id and order_id.isdigit() and user_id and user_id.isdigit()):
+#         #     return jsonify({"message": "Missing or invalid query parameters."}), 400
+#         # SQL query to fetch order details
+#         query = """
+#             SELECT  
+#                 A.SlNo,
+#                 A.OrderType,
+#                 A.LocationID,
+#                 A.CustCode,
+#                 A.NetAmount,
+#                 A.MakingTime,
+#                 A.MakerId,
+#                 C.Location AS LocationName,
+#                 B.CardName AS CustomerName,
+#                 (
+#                     SELECT 
+#                         D.ProductCode,
+#                         D.ProdName,
+#                         D.Qty,
+#                         D.DiscountPerc,
+#                         D.LineTotal
+#                     FROM 
+#                         TBL_SalesOrderProductDetails AS D
+#                     WHERE 
+#                         D.SOID = A.SlNo
+#                     FOR JSON PATH
+#                 ) AS Products
+#             FROM 
+#                 TBL_SalesOrderDetails AS A
+#             LEFT JOIN CustomerMaster_M_Tbl AS B ON A.CustCode = B.CardCode
+#             LEFT JOIN LocationMaster_M_Tbl AS C ON A.LocationID = C.LocationId
+#             LEFT JOIN TBL_Users AS D ON D.UserID = A.MakerId
+#             WHERE 
+#                 A.Status IN ('N' ,'P') AND A.SlNo = ? AND D.Usr_Name = ?
+#             FOR JSON PATH, INCLUDE_NULL_VALUES;
+#         """
+#         params = (order_id, user_id)
+#         # Execute the query and fetch the result
+#         orderdetails = ms_query_db(query, params)
+#         # Process the result
+#         if orderdetails:
+#             json_key = "JSON_F52E2B61-18A1-11d1-B105-00805F49916B"
+#             raw_data = json.loads(orderdetails[0].get(json_key, "[]"))
+#             formatted_data = (
+#                 raw_data[0] if isinstance(raw_data, list) and raw_data else {}
+#             )
+#         else:
+#             formatted_data = {}
+#         # check formatted_data is empty or not
+#         if not formatted_data:
+#             return jsonify({"message": "No data found."}), 404
+#         return jsonify(formatted_data), 200
+#     except Exception as e:
+#         print(f"Error: {e}")
+#         return jsonify({"message": "Internal server error."}), 500
     
 @order_bp.route("/orderApprove", methods=["POST"])
 def approve_order():
--- setting/config.py
+++ setting/config.py
@@ -4,7 +4,8 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "your_secret_key"
     DATABASE_URI = (
-        os.environ.get("DATABASE_URL")
-        or "postgresql://postgres:George%40789@192.168.168.222:5432/eBizAPI"
+        # os.environ.get("DATABASE_URL")
+        "postgresql://go_pg_16:G30600f%40wall@192.168.168.126:5432/GeoOne"
+        # or "postgresql://postgres:George%40789@192.168.168.222:5432/eBizAPI"
     )
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "your_jwt_secret_key"
