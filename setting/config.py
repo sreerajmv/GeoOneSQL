2ceebb3a836d19c63c31723df223a7438fe33c77
sreeraj.mv <sreeraj.mv@gmail.com>
2026-05-25 15:12:12 +0530

    updated

--- setting/config.py
+++ setting/config.py
@@ -3,9 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "your_secret_key"
-    DATABASE_URI = (
-        # os.environ.get("DATABASE_URL")
-        "postgresql://go_pg_16:G30600f%40wall@192.168.168.126:5432/GeoOne"
-        # or "postgresql://postgres:George%40789@192.168.168.222:5432/eBizAPI"
+    DATABASE_URI = os.environ.get(
+        "DATABASE_URI", "postgresql://default_user:password@localhost/default_db"
     )
     JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY") or "your_jwt_secret_key"
